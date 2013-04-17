994da5ab1081311eb221d924d7ce1ce8e2e7cecb
nerthul <nerthul_13@hotmail.com>
2013-04-17 08:51:43 +0200

    conseguido hacer una prueba con node.js

--- www/js/app.js
+++ www/js/app.js
@@ -1,8 +1,14 @@
+// note, io.listen(<port>) will create a http server for you
 var io = require('socket.io').listen(80);
 
 io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
+  io.sockets.emit('this', { will: 'be received by everyone'});
+
+  socket.on('private message', function (from, msg) {
+    console.log('I received a private message by ', from, ' saying ', msg);
+  });
+
+  socket.on('disconnect', function () {
+    io.sockets.emit('user disconnected');
   });
 });
