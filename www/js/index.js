76eecdbcf2ae27afe87bedac139b99f5a5bbae20
Carlos Gomez <superdjin@gmail.com>
2013-05-10 14:27:08 +0200

    mejoras interfaz

--- www/js/index.js
+++ www/js/index.js
@@ -29,14 +29,35 @@ $('.leftLink').parent().parent().parent().removeClass('ui-btn');
 $('.leftLink').contents().unwrap();
 });
 
+$(function() {
+        	// Bind the tapHandler callback function to the tap event on div.box
+            $( ".botonVotar" ).on( 'tap', tapHandler );
+ 
+            // Callback function references the event target and adds the 'tap' class to it
+            function tapHandler( event ) {
+        			$(event.target).addClass( "tap" );
+					$(event.target).buttonMarkup( "refresh" );
+        		}
+      });
+
 function anadirLista(){
 $('#contenido').replaceWith("<ul id=\"lista \" data-role=\"listview\"></ul>");
 };
 
 function anadirCancion(nombre,artista,album,id,duracion,votado,sonado){
 
+var pulsado;
+if(votado==1){
+pulsado
+}
 
-
-$('#lista').html("")
+$('#lista').html("<li id=\""+id+"\" data-icon=\"star\" >" +
+				"<a href=\"#\" onclick=\"return false;\" class=\"leftLink\" >" + 
+					"<span class=\"ui-link-inherit\" >" + 
+						"<h1>"+nombre+"</h1><p>"+artista+"</p><p>"+album+"</p>" + 
+					"</span>" +
+				"</a>" +
+				"<a href=\"#\" data-theme=\"a\"</a>" +
+			"</li>")
 
 }
