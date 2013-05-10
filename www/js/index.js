9d45de890bcc39c090440016d07038e7a9aad71b
Carlos Gomez <superdjin@gmail.com>
2013-05-10 17:58:57 +0200

    avanzado

--- www/js/index.js
+++ www/js/index.js
@@ -1,16 +1,16 @@
 var app = {
-    initialize: function() {
+    initialize: function () {
         this.bind();
     },
-    bind: function() {
+    bind: function () {
         document.addEventListener('deviceready', this.deviceready, false);
     },
-    deviceready: function() {
+    deviceready: function () {
         // This is an event handler function, which means the scope is the event.
         // So, we must explicitly called `app.report()` instead of `this.report()`.
         app.report('deviceready');
     },
-    report: function(id) {
+    report: function (id) {
         // Report the event in the console
         console.log("Report: " + id);
 
@@ -24,40 +24,64 @@ var app = {
 };
 
 //Codigo para quitar el link izquierdo de los split button de las listview
-$(document).ready(function(){
-$('.leftLink').parent().parent().parent().removeClass('ui-btn');
-$('.leftLink').contents().unwrap();
+$(document).ready(function () {
+    $('.leftLink').parent().parent().parent().removeClass('ui-btn');
+    $('.leftLink').contents().unwrap();
+    anadirLista();
+    anadirCancion("asdgasdgasdg", "Siemasasdgasdgpre Fuertes", "dsdg", "1", "1:30", 0, 0);
+    /////// ??????????????????
 });
 
-$(function() {
-        	// Bind the tapHandler callback function to the tap event on div.box
-            $( ".botonVotar" ).on( 'tap', tapHandler );
- 
-            // Callback function references the event target and adds the 'tap' class to it
-            function tapHandler( event ) {
-        			$(event.target).addClass( "tap" );
-					$(event.target).buttonMarkup( "refresh" );
-        		}
-      });
+$(function () {
+    // Bind the tapHandler callback function to the tap event on div.box
+    $(".botonVotar").on('tap', tapHandler);
 
-function anadirLista(){
-$('#contenido').replaceWith("<ul id=\"lista \" data-role=\"listview\"></ul>");
+    // Callback function references the event target and adds the 'tap' class to it
+    function tapHandler(event) {
+        cambiarIconoVotada();
+    }
+});
+
+function anadirLista() {
+    $('#contenido').empty();
+    $('#contenido').html("<ul id=\"lista\" data-role=\"listview\"></ul>");
+    anadirCancion("Never Gonna", "Siempre Fuertes", "HUEAHEUA", "1", "1:30", 0, 0);
+    
 };
 
-function anadirCancion(nombre,artista,album,id,duracion,votado,sonado){
+function anadirCancion(nombre, artista, album, id, duracion, votado, sonado) {
 
-var pulsado;
-if(votado==1){
-pulsado
-}
+    var pulsado;
+    if (votado == 1) {
+        pulsado
+    }
 
-$('#lista').html("<li id=\""+id+"\" data-icon=\"star\" >" +
-				"<a href=\"#\" onclick=\"return false;\" class=\"leftLink\" >" + 
-					"<span class=\"ui-link-inherit\" >" + 
-						"<h1>"+nombre+"</h1><p>"+artista+"</p><p>"+album+"</p>" + 
+    var textoAnadir = "<li id=\"" + id + "\" data-icon=\"star\">" +
+				"<a href=\"#\"onclick=\"return false;\" class=\"leftLink\">" +
+					"<span class=\"ui-link-inherit\">" +
+						"<h1>" + nombre + "</h1><p>" + artista + "</p><p>" + album + "</p>" +
 					"</span>" +
 				"</a>" +
-				"<a href=\"#\" data-theme=\"a\"</a>" +
-			"</li>")
+				"<a href=\"#\" data-theme=\"a\"></a>" +
+			"</li>";
 
+    $('#lista').append(textoAnadir);
+
+    $('#lista').listview("refresh");
+    /*
+    <li id="idcancion" data-icon="star">
+    <a href="#" onclick="return false;" class="leftLink">
+    <span class="ui-link-inherit">
+    <h1>nombrecancion</h1><p>artistacancion</p><p>albumcancion</p>
+    </span>
+    </a>
+    <a href="#" data-theme="a"></a>
+    </li> */
+};
+
+
+//Funcion para cambiar el icono a una cancion votada. Recibe el id de la cancion y la transforma al selector jquery.
+function cambiarIconoVotada(id) {
+    var selector = "#" + id + " .ui-btn-inner .ui-btn .ui-icon";
+    $(selector).addClass("ui-icon-check").removeClass("ui-icon-star");
 }
